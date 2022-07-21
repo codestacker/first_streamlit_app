da10167f9dce2a4f1512ddf9bdb3d35f1023209e
codestacker <codestacker@users.noreply.github.com>
2022-07-21 22:04:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Pasta')
-streamlit.text('Paratha')
-streamlit.text('Poha')
+streamlit.text('🥣 Pasta')
+streamlit.text(' 🥗 Salad')
+streamlit.text('🥑🍞 Avacado toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
