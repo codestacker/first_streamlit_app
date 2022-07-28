2914112bc25964c2dde207f42415caf7e016c887
codestacker <codestacker@users.noreply.github.com>
2022-07-28 16:43:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 # json version of the response and normalize it
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
+# output the table
+streamlit.dataframe(fruityvice_normalized)
+
+streamlit.header("Fruityvice Fruit Advice!")
+
 fruit_choice_1 = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('The user entered ', fruit_choice)
 
